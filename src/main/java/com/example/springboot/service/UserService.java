26ef41f1add494e5f2de49e87e38abc2172b12ec
ZHUXIANG\12289 <1228910172@qq.com>
2025-05-14 21:53:56 +0800

    测试

--- src/main/java/com/example/springboot/service/UserService.java
+++ src/main/java/com/example/springboot/service/UserService.java
@@ -24,7 +24,7 @@ public class UserService {
     }
 
     public List<User> getAllUsers() {
-        logger.info("Fetching all");
+        logger.info("Fetching");
         return userRepository.findAll();
     }
 
